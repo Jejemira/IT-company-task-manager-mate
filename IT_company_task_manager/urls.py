ce452b3f1a4c7dc5798dff4f9fae48d1c005bf4e
Tkachov Mykhailo <mtkachov435@gmail.com>
2025-09-16 15:33:07 +0300

    add path for login

--- IT_company_task_manager/urls.py
+++ IT_company_task_manager/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task.urls", namespace="task")),
-    # path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     # path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
